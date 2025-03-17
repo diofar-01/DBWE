a042aa5bd3de00d9ab487068e1c243b3872f7a24
Diogo Oliveira <diogo.faria15@hotmail.com>
2025-03-17 13:51:21 +0100

    Add new files

--- project/__init__.py
+++ project/__init__.py
@@ -1,14 +1,4 @@
 # init.py
-from flask_migrate import Migrate
-migrate = Migrate()
-
-def create_app():
-    app = Flask(__name__)
-    # ... deine Konfiguration und db.init_app(app)
-    migrate.init_app(app, db)
-    # ... Registrierung deiner Blueprints etc.
-    return app
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
@@ -25,6 +15,7 @@ def create_app():
     db.init_app(app)
 
     login_manager = LoginManager()
+    login_manager.login_message = None
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
